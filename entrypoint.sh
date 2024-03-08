19687dda2dfd68f257462224f234901804150c39
Monius <m1105930979@gmail.com>
2024-03-08 23:20:53 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ echo $MICRO_MAMBA
 curl -fsSL $MICRO_MAMBA | tar -xvj bin/micromamba
 mv bin/micromamba /usr/bin/mamba
 /usr/bin/mamba shell init -s bash -p $DEV_PATH/mamba
-# . "$HOME/.bashrc"
+. "$HOME/.bashrc"
 
 micromamba create -n aii python=3.11 gradio -c conda-forge -y
 micromamba activate aii
