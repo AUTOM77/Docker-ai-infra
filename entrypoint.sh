49925a054117a007863d6c31ad404228453908ae
Monius <m1105930979@gmail.com>
2024-06-04 03:15:12 +0800

    fuck huggingface

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,11 +28,13 @@ fi
 
 echo "========= log: $USER ========="
 
+echo "mamba run pip install --no-cache-dir -r /opt/run/ai-infra.txt" > /usr/bin/ai-infra
+
 if [ -n "$AI_SOCKET" ]; then
     # echo "mamba run hypercorn -b unix:$AI_SOCKET -w 4 /opt/run/app:app" > /usr/bin/ai-infra
-    echo "mamba run hypercorn -b unix:$AI_SOCKET /opt/run/app:app" > /usr/bin/ai-infra
+    echo "mamba run hypercorn -b unix:$AI_SOCKET /opt/run/app:app" >> /usr/bin/ai-infra
 else
-    echo "mamba run hypercorn -b '[::]:9100' /opt/run/app:app"  > /usr/bin/ai-infra
+    echo "mamba run hypercorn -b '[::]:9100' /opt/run/app:app" >> /usr/bin/ai-infra
 fi
 
 exec "$@"
